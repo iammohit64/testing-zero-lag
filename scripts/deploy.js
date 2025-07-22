c13881e4ecc1d8b6635c61a4ba8799ce9b99d6e2
Mohit <mohit11994cu@gmail.com>
2025-07-23 00:41:22 +0530

    Updated Smart Contract to Main to test it

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,17 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
 
 async function main() {
-  const ZeroLagTask = await ethers.getContractFactory("ZeroLagTask");
+  const [deployer] = await hre.ethers.getSigners();
 
-  const zeroLag = await ZeroLagTask.deploy();
-  await zeroLag.waitForDeployment();
+  const platformWallet = deployer.address; // Replace if you want another wallet
+  const minimumStake = hre.ethers.parseEther("0.01"); // 0.01 ETH minimum
 
-  console.log("ZeroLagTask deployed to:", await zeroLag.getAddress());
+  const TaskStaking = await hre.ethers.getContractFactory("TaskStaking");
+  const taskStaking = await TaskStaking.deploy(platformWallet, minimumStake);
+
+  await taskStaking.waitForDeployment();
+
+  console.log("TaskStaking deployed to:", await taskStaking.getAddress());
 }
 
 main().catch((error) => {
